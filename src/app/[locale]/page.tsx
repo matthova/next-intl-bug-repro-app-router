9aced284c9faf949fd06488787b2788d0db3b150
Matt Hova <matthova87@gmail.com>
2024-11-21 21:04:08 -0800

    minimal repro

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,12 @@
 import {useTranslations} from 'next-intl';
+import {Link} from '@/src/i18n/routing';
 
 export default function IndexPage() {
   const t = useTranslations('IndexPage');
-  return <h1>{t('title')}</h1>;
+  return (
+    <>
+      <h1>{t('title')}</h1>
+      <Link href="/about">{t('linkDescription')}</Link>
+    </>
+  );
 }
